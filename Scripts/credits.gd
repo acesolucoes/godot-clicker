d9a59e3e551ec87a36469688e76f60c8978ee2ab
Edward Boszczowski <edward.boszczowski@pixeon.com>
2026-05-05 19:15:07 -0300

    adding credits scene

--- Scripts/credits.gd
+++ Scripts/credits.gd
@@ -1,8 +1,8 @@
 extends Label
 
-var level2 = preload("res://Scenes/level.tscn")
+var creditScene = preload("res://Scenes/credits/credits.tscn")
 
 func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.pressed:
-			get_tree().change_scene_to_packed(level2)
+			get_tree().change_scene_to_packed(creditScene)
